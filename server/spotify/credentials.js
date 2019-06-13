5b38f41df7e5bee98477e123c622d85ee040b257
João Justo <jpjustonunes@gmail.com>
2019-06-13 19:05:18 +0100

    initial commit

--- server/spotify/credentials.js
+++ server/spotify/credentials.js
@@ -1,5 +1,5 @@
 module.exports = {
-  client_id: '3702fb155a64454e850898d42cb25f60',
-  client_secret: '0e1fcf2387a54465b705e5f4518af626',
-  redirect_uri: 'http://localhost:5000/callback'
+  client_id: "your_client_id",
+  client_secret: "your_client_secret",
+  redirect_uri: "http://localhost:5000/callback"
 };
